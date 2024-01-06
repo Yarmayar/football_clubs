06b1b8524ac1bf313f0620b60e9e9350b5159efa
Yarmayar <my4and@gmail.com>
2024-01-07 01:53:37 +0300

    Добавляем аутентификацию через e-mail/password, добавив бэкенд на основе базового - метод authenticate

--- football_clubs_project/football_clubs_project/settings.py
+++ football_clubs_project/football_clubs_project/settings.py
@@ -135,6 +135,12 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'users:login'
 LOGIN_URL = 'users:login'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'users.authentication.EmailAuthBackend'
+
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
