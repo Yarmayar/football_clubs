f2f36ec57a3517e6c2306ca50bed64d3d0b38b1c
Yarmayar <my4and@gmail.com>
2024-01-09 16:32:57 +0300

    Настройка почтового сервера SMTP

--- football_clubs_project/football_clubs_project/settings.py
+++ football_clubs_project/football_clubs_project/settings.py
@@ -142,8 +142,26 @@ AUTHENTICATION_BACKENDS = [
 
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+# EMAIL_HOST = 'smtp.yandex.ru'
+# EMAIL_PORT = 465
+# EMAIL_HOST_USER = ' '
+# EMAIL_HOST_PASSWORD = ' '
+# EMAIL_USE_SSL = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = ' '
+EMAIL_HOST_PASSWORD = ' '
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
