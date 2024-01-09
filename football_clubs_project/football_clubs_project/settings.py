3678da3d5d8ab77b05610ae56108e6cfdead3168
Yarmayar <my4and@gmail.com>
2024-01-09 14:49:59 +0300

    Создать (+) password_reset_form для отображения формы восст пароля (+) password_reset_done с сообщением от отправке письма (+) password_reset_confirm для формы ввода нового пароля (+) password_reset_complete для сообщ об успеш. обновл. пароля (+) password_reset_email для использования шаблона из нашего простр имен {% url 'users:password_reset_confirm' uidb64=uid token=token %}

--- football_clubs_project/football_clubs_project/settings.py
+++ football_clubs_project/football_clubs_project/settings.py
@@ -138,9 +138,11 @@ LOGIN_URL = 'users:login'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'users.authentication.EmailAuthBackend'
+    'users.authentication.EmailAuthBackend',
 
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- football_clubs_project/users/urls.py
+++ football_clubs_project/users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from django.contrib.auth.views import LogoutView, PasswordChangeDoneView
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LogoutView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
 
 from users import views
 
@@ -11,8 +12,27 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
-    path('password_change/', views.ChangePassword.as_view(title='Changing password'), name='password_change'),
-    path('password_change/done/', views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html',
+    path('password-change/', views.ChangePassword.as_view(title='Changing password'), name='password_change'),
+    path('password-change/done/', views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html',
                                                                        title='New password created'),
          name='password_change_done'),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html',
+                                                      email_template_name='users/password_reset_email.html',
+                                                      success_url=reverse_lazy('users:password_reset_done'),
+                                                      title='Reset password'),
+                                                      name='password_reset'),
+    path('password-reset/done/', PasswordResetView.as_view(
+        template_name='users/password_reset_done.html',
+        title='Reset password'),
+        name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html',
+        success_url=reverse_lazy('users:password_reset_complete'),
+        title='Create new password'),
+        name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html',
+        title="New password's created"),
+        name='password_reset_complete'),
+
 ]
